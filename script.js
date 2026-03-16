b3634025bab01befa25b8a7c90e92a39989576e5
Javier Bradlim <144743434+jbradlim08@users.noreply.github.com>
2026-03-16 17:16:32 -0500

    Finish moveTrain() and drawTrain(). These allow the train to be displayed on the canvas and move logically

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 
 const trainColor = 'lightgreen';
-const trainBorder = 'black';
+const trainBorderColor = 'black';
 const waterColor = 'red';
 const backgroundColor = '#378BAF';
 
@@ -19,7 +19,10 @@ let xVelocity = unitSize; // (-) to left and (+) to right
 let yVelocity = 0;
 let running = false;
 
-let waterX, waterY;
+let waterPos = {
+    x: 0,
+    y: 0
+}
 
 let train = [ // Default value
     {x:unitSize * 3, y:0},
@@ -41,17 +44,25 @@ function gameStart(){
     cleanWaterScoreText.textContent = cleanWaterScore;
     createWater();
     drawWater();
-    drawTrain();
     nextTick();
 }
 
 function nextTick(){
     if(running){
         setTimeout(() => {
+            // clear canvas
+            clearCanvas();
             moveTrain();
+
+            // redraw canvas
+            drawWater();
             drawTrain();
+
+            // check game over
             checkGameOver();
-            nextTick(); // recursion
+
+             // recurse back
+            nextTick();
         }, 50)
     } else{
         displayGameOver();
@@ -59,7 +70,7 @@ function nextTick(){
 }
 
 // Repainting the board
-function clearBoard(){ 
+function clearCanvas(){ 
     ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, gameWidth, gameHeight)
 }
@@ -70,21 +81,51 @@ function createWater(){
         return Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
     }
 
-    waterX = randomWater(0, gameWidth - unitSize);
-    waterY = randomWater(0, gameWidth - unitSize);
+    waterPos.x = randomWater(0, gameWidth - unitSize);
+    waterPos.y = randomWater(0, gameWidth - unitSize);
 }
 
 function drawWater(){
     ctx.fillStyle = waterColor;
-    ctx.fillRect(waterX, waterY, unitSize, unitSize);
+    ctx.fillRect(waterPos.x, waterPos.y, unitSize, unitSize);
 }
 
 function moveTrain(){
+    const head = {
+        x: train[0].x + xVelocity,
+        y: train[0].y + yVelocity
+    };
 
+    // To add in front
+    train.unshift(head);
+
+    // check collision
+    checkTrainCollision();
+}
+
+function checkTrainCollision(){
+    if(train[0].x == waterPos.x && train[0].y == waterPos.y){
+        cleanWaterScore++;
+        cleanWaterScoreText.text = cleanWaterScore;
+        createWater();
+    } else{
+        train.pop();
+    }
 }
 
 function drawTrain(){
-
+    ctx.fillStyle = trainColor;
+    ctx.strokeStyle = trainBorderColor;
+    train.forEach((trainPart) => {
+        ctx.fillRect(trainPart.x, 
+                    trainPart.y, 
+                    unitSize, 
+                    unitSize)
+        ctx.strokeRect(trainPart.x, 
+                    trainPart.y, 
+                    unitSize, 
+                    unitSize)
+    })
 }
 
 function changeDirection(){
